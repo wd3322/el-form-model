7be7095b246f3a786121cd469a441eeeedf57c31
wudi <wd87269288@163.com>
2023-05-29 16:20:21 +0800

    version 1.6.3

--- src/index.js
+++ src/index.js
@@ -4,33 +4,35 @@ import DefaultAttrs from './attrs'
 import './style.scss'
 
 export default {
-  install(Vue, attrs = {}) {
-    const errorHeader = 'el-form-model install config'
-    if (Utils.getPrototype(attrs) !== 'object') {
-      throw new Error(`${errorHeader} is not a object`)
+  install(Vue, attrs) {
+    if (attrs) {
+      const errorHeader = 'el-form-model install config'
+      if (Utils.getPrototype(attrs) !== 'object') {
+        throw new Error(`${errorHeader} is not a object`)
+      }
+      if (Utils.getPrototype(attrs.global) !== 'object') {
+        throw new Error(`${errorHeader} 'global' is not a object`)
+      }
+      if (Utils.getPrototype(attrs.global.placeholder) !== 'object') {
+        throw new Error(`${errorHeader} 'global.placeholder' is not a object`)
+      }
+      if (Utils.getPrototype(attrs.global.placeholder.input) !== 'string') {
+        throw new Error(`${errorHeader} 'global.placeholder.input' is not a string`)
+      }
+      if (Utils.getPrototype(attrs.global.placeholder.select) !== 'string') {
+        throw new Error(`${errorHeader} 'global.placeholder.select' is not a string`)
+      }
+      if (Utils.getPrototype(attrs.component) !== 'object') {
+        throw new Error(`${errorHeader} 'component' is not a object`)
+      }
+      if (!['object', 'function', 'asyncfunction'].includes(Utils.getPrototype(attrs.component.form))) {
+        throw new Error(`${errorHeader} 'component.form' is not a object or function`)
+      }
+      if (!['object', 'function', 'asyncfunction'].includes(Utils.getPrototype(attrs.component.formItem))) {
+        throw new Error(`${errorHeader} 'component.formItem' is not a object or function`)
+      }
     }
-    if (Utils.getPrototype(attrs.global) !== 'object') {
-      throw new Error(`${errorHeader} 'global' is not a object`)
-    }
-    if (Utils.getPrototype(attrs.global.placeholder) !== 'object') {
-      throw new Error(`${errorHeader} 'global.placeholder' is not a object`)
-    }
-    if (Utils.getPrototype(attrs.global.placeholder.input) !== 'string') {
-      throw new Error(`${errorHeader} 'global.placeholder.input' is not a string`)
-    }
-    if (Utils.getPrototype(attrs.global.placeholder.select) !== 'string') {
-      throw new Error(`${errorHeader} 'global.placeholder.select' is not a string`)
-    }
-    if (Utils.getPrototype(attrs.component) !== 'object') {
-      throw new Error(`${errorHeader} 'component' is not a object`)
-    }
-    if (!['object', 'function', 'asyncfunction'].includes(Utils.getPrototype(attrs.component.form))) {
-      throw new Error(`${errorHeader} 'component.form' is not a object or function`)
-    }
-    if (!['object', 'function', 'asyncfunction'].includes(Utils.getPrototype(attrs.component.formItem))) {
-      throw new Error(`${errorHeader} 'component.formItem' is not a object or function`)
-    }
-    const mixin = DefaultAttrs.get(attrs)
+    const mixin = DefaultAttrs.get(attrs || {})
     Vue.mixin(mixin)
     Vue.component('el-form-model', ElFormModel)
   }
